422462ca23c1191f88e55485be98d8aad5fe1583
Rafael Dimas <rafael.dimas@live.com>
2022-08-04 22:12:31 -0300

    Configurado Projeto

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  testEnvironment: 'node',
+  preset: 'ts-jest'
+}
